12a3d93df9bca935c1ad15097ef88c15f66e733a
Vova <savinvova21@gmail.com>
2023-02-24 20:49:22 +0200

    file tsx

--- src/index.tsx
+++ src/index.tsx
@@ -11,5 +11,20 @@ import ReactDOM from 'react-dom/client'
 
 const title = <h1 className='red'>Test</h1>
 
+const list = (
+  <ul>
+    <li>List item1</li>
+    <li>List item2</li>
+    <li>List item3</li>
+  </ul>
+)
+
+const content = (
+  <div>
+    {title}
+    {list}
+  </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{content}</React.StrictMode>)
