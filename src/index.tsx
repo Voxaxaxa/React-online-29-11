2fe07d95428f62653e30f99e973e5c65773918a5
Vova <savinvova21@gmail.com>
2023-02-27 20:44:48 +0200

    file Content

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Title from './Title'
-
-const Content = () => {
-    return (
-        <React.Fragment>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aspernatur aperiam placeat vitae dignissimos maiores eaque
-                asperiores natus neque amet ad. Consectetur blanditiis nihil
-                adipisci totam? At minus error itaque tempore?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-                eius harum minima et repellat esse ratione alias neque cum
-                nesciunt ab debitis, molestias beatae rem, molestiae sapiente
-                doloremque voluptates accusantium.
-            </p>
-        </React.Fragment>
-    )
-}
+import Content from './Content'
 
 const App = () => {
     return (
         <>
             <Title title="React" nums={2022} />
             <Title title="Add" nums={2023} />
-            <Content />
+            <Content text1="Hello world text1" text2="Hello world text2" />
         </>
     )
 }
