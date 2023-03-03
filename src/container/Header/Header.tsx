110c735046de87b156bf8a566aa5593b74620097
Vova <savinvova21@gmail.com>
2023-03-03 20:34:53 +0200

    style2

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -5,11 +5,12 @@ import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
+import './Header.css'
 
 type Props = {}
 const Header = (props: Props) => {
     return (
-        <AppBar position="static" sx={{ backgroundColor: 'green' }}>
+        <AppBar position="static" className="app-bar">
             <Container>
                 <Toolbar>
                     <IconButton
