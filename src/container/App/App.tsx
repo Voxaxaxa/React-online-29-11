532747e6a4027f6e7ea6c9f6273a4aeca01053fc
Vova <savinvova21@gmail.com>
2023-03-03 19:47:11 +0200

    CSS

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,15 +1,19 @@
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
+
 const App = (props: Props) => {
     return (
-        <div>
+        <>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
-        </div>
+        </>
     )
 }
+
 export default App
